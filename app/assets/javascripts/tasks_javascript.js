7ca47ab8daebe6505481590b547aad974c61fc35
nate dawg <nglass1988@gmail.com>
2014-06-10 15:39:51 -0700

    Fix issues with task searching

--- app/assets/javascripts/tasks_javascript.js
+++ app/assets/javascripts/tasks_javascript.js
@@ -1,19 +1,34 @@
 // You can use CoffeScript in tasks.js.coffee if you'd rather.
-function updateTask(e, task, attr, node) {
-	var updateObj = {};
-	updateObj[attr] = !task[attr];
-	$.ajax({
-		url: "tasks/" + task.id + ".json",
-		type: "PUT",
-		contentType: "application/json",
-		dataType: "json",
-		data: JSON.stringify(updateObj),
-		success: function() {
-			if (attr == "complete") {
-				var row = $(node).closest("tr");
-				row.toggleClass("completed");
+// $(document).ready(function() {
+	// var parser = new DOMParser();
+	// $(".edit").each(function(index, elem) {
+	// 	$(elem).on("click", htmlToString);
+	// });
+
+	// function htmlToString() {
+	// 	console.log("asjnfjksdf", this);
+	// 	var parsed = $(parser.parseFromString($("#task_title").val(), "text/html"));
+	// 	var newStr = parsed.find("body").text();
+	// 	$("#task_title").val(newStr);
+	// }
+
+	function updateTask(e, task, attr, node) {
+		console.log("updateTask");
+		var updateObj = {};
+		updateObj[attr] = !task[attr];
+		$.ajax({
+			url: "tasks/" + task.id + ".json",
+			type: "PUT",
+			contentType: "application/json",
+			dataType: "json",
+			data: JSON.stringify(updateObj),
+			success: function() {
+				if (attr == "complete") {
+					var row = $(node).closest("tr");
+					row.toggleClass("completed");
+				}
 			}
-		}
-	});
-	// e.preventDefault();
-};
+		});
+	};
+// });
+
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  # after_initialize :default_values
+  # before_save :not_null
   {
     complete: { completed: true, incomplete: false },
     archived: { archived: true, unarchived: false }
@@ -10,6 +12,21 @@ class Task < ActiveRecord::Base
     define_method "#{attribute}!" do
       update_attribute(attribute, true)
     end
+  end
+
+  # def default_values
+  #   self.complete ||= false
+  #   self.archived ||= false
+  # end
+
+  # def not_null
+  #   # if self.complete == 
+  # end
+
+  def self.search(query)
+    title = where("description like ?", "%#{query}%")
+    desc = where("title like ?", "%#{query}%")
+    title | desc
   end
 
 end
