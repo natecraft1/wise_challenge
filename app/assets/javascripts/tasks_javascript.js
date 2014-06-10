bdefa5a82b76f939f745915aad8741af38e8426f
nate dawg <nglass1988@gmail.com>
2014-06-09 20:34:01 -0700

    Style completed tasks

--- app/assets/javascripts/tasks_javascript.js
+++ app/assets/javascripts/tasks_javascript.js
@@ -1,10 +1,14 @@
 // You can use CoffeScript in tasks.js.coffee if you'd rather.
-function completeTask(taskID, status) {
+function completeTask(taskID, status, e, node) {
 	$.ajax({
 		url: "tasks/" + taskID,
 		type: "PUT",
 		contentType: "application/json",
 		dataType: "json",
-		data: JSON.stringify({"complete": !status})
+		data: JSON.stringify({"complete": !status}),
+		success: function() {
+			var row = $(node).closest("tr");
+			row.toggleClass("completed");
+		}
 	});
 };
